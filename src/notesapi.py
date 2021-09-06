5cc31c643f7d94ab7c0ca26245acd0bb5b8c8e63
Ejaj Khan <ejaj1707@gmail.com>
2021-09-06 13:40:50 +0600

    2nd Commit - marsmallow depndancy removed

--- src/notesapi.py
+++ src/notesapi.py
@@ -1,4 +1,5 @@
 
+from datetime import datetime, time
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -7,6 +8,7 @@ import os
 # Init app
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SECRET_kEY'] = 'aronno'
 # Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -20,21 +22,15 @@ class Note(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(100), unique=True)
   body = db.Column(db.String(200))
+  creationTime = db.Column(db.DateTime, default=datetime.utcnow)
 
-  def __init__(self, name, body):
-    self.name = name
-    self.body = body
-
-
-# Note Schema
-class NoteSchema(ma.Schema):
-  class Meta:
-    fields = ('id', 'name', 'body')
-
-
-# Init schema
-note_schema = NoteSchema()
-notes_schema = NoteSchema(many=True)
+  def to_dict(self):
+    return dict(
+      id=self.id,
+      name=self.name,
+      body=self.body,
+      creationTime= self.creationTime,
+    )
 
 db.create_all()
 
@@ -42,30 +38,27 @@ db.create_all()
 # Create a Note
 @app.route('/note', methods=['POST'])
 def add_note():
-  name = request.json['name']
-  body = request.json['body']
+  data=request.get_json()
+  note = Note(name=data['name'],body=data['body'])
 
-  new_note = Note(name, body)
-
-  db.session.add(new_note)
+  db.session.add(note)
   db.session.commit()
 
-  return note_schema.jsonify(new_note)
+  return jsonify(note.to_dict())
 
 
 # Get All notes
 @app.route('/note', methods=['GET'])
 def get_notes():
   all_notes = Note.query.all()
-  result = notes_schema.dump(all_notes)
-  return jsonify(result)
+  return jsonify([n.to_dict() for n in all_notes])
 
 
 # Get Single notes
 @app.route('/note/<id>', methods=['GET'])
 def get_note(id):
   note = Note.query.get(id)
-  return note_schema.jsonify(note)
+  return jsonify([ note.to_dict() ])
 
 
 # Update a Note
@@ -78,11 +71,9 @@ def update_note(id):
 
   note.name = name
   note.body = body
-
-
   db.session.commit()
-
-  return note_schema.jsonify(note)
+  
+  return jsonify([note.to_dict()])
 
 
 # Delete note
@@ -92,7 +83,7 @@ def delete_note(id):
   db.session.delete(note)
   db.session.commit()
 
-  return note_schema.jsonify(note)
+  return jsonify(note.to_dict())
 
 # Run Server
 if __name__ == '__main__':
